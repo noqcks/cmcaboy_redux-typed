793c248e6b3065b2c0b01403bd05639c6a4794b6
noqcks <benji@093b.org>
2024-04-07 01:51:49 +0000

    Upgrade dependencies

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, NavLink } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 import HomePage from "../components/HomePage";
 
@@ -8,13 +8,13 @@ export const history = createHistory();
 // Instead of BrowserRouter, we use the regular router,
 // but we pass in a customer history to it.
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router>
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
