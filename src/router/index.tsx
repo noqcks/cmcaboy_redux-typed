4ce35a5268acfc64fad3996b06da92fe8858bed2
noqcks <benji@093b.org>
2024-04-07 01:48:28 +0000

    Upgrade dependencies

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link, NavLink } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 import HomePage from "../components/HomePage";
 
