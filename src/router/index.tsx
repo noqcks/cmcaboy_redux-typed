b0c5fd57b1e20681116ed2d1bd5b26670f8027d8
noqcks <benji@093b.org>
2024-04-07 01:49:58 +0000

    Upgrade dependencies

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,7 +8,7 @@ export const history = createHistory();
 // Instead of BrowserRouter, we use the regular router,
 // but we pass in a customer history to it.
 const AppRouter = () => (
-  <Router>
+  <BrowserRouter>
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
